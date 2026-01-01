683e988be2137d6f5434af03f62507cd75dd8b41
Volodya <gavr.22898@gmail.com>
2026-01-01 20:57:11 +0500

    complete auth page frontend

--- frontend/app/pages/auth/register.vue
+++ frontend/app/pages/auth/register.vue
@@ -1,5 +1,14 @@
 <script setup>
+const isLoginPage = ref(true);
+
+const isPasswordVisible = ref(false);
+
+const togglePassword = () => {
+  isPasswordVisible.value = !isPasswordVisible.value;
+};
+
 const number = ref("+7");
+const password = ref("");
 
 watch(number, (newVal, oldVal) => {
   if (!newVal.startsWith("+7")) {
@@ -24,13 +33,29 @@ watch(number, (newVal, oldVal) => {
 <template>
   <div class="register">
     <div class="register__block">
-      <h1 class="register__title">Регистрация</h1>
+      <h1 class="register__title">
+        {{ isLoginPage ? "Вход" : "Регистрация" }}
+      </h1>
+
       <input class="register__input" type="text" v-model="number" />
-      <input
-        class="register__input"
-        type="password"
-        placeholder="Придумайте пароль"
-      />
+
+      <div class="register__password">
+        <input
+          v-model="password"
+          class="register__input"
+          :type="isPasswordVisible ? 'text' : 'password'"
+          :placeholder="isLoginPage ? 'Введите пароль' : 'Придумайте пароль'"
+        />
+        <button
+          type="button"
+          @click="togglePassword"
+          class="register__eye"
+        ></button>
+      </div>
+
+      <p class="register__toggle" @click="isLoginPage = !isLoginPage">
+        {{ isLoginPage ? "Ещё нет аккаунта?" : "Уже есть аккаунт?" }}
+      </p>
       <button class="register__button">Продолжить</button>
     </div>
   </div>
@@ -60,7 +85,7 @@ watch(number, (newVal, oldVal) => {
     height: 40px;
     border-radius: $border-radius;
     margin: 0 0 15px 0;
-    padding: 0 0 0 20px;
+    padding: 0 40px 0 20px;
     border: 1px solid $accent;
 
     &:focus {
@@ -74,6 +99,37 @@ watch(number, (newVal, oldVal) => {
       font-size: 12px;
       font-weight: 400;
     }
+  }
+  &__password {
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  &__eye {
+    position: absolute;
+    width: 22px;
+    height: 22px;
+    right: 27px;
+    top: 9px;
+    background-color: $text-primary;
+
+    mask-image: url("/images/eye.svg");
+    mask-repeat: no-repeat;
+    mask-position: center;
+    mask-size: contain;
+    -webkit-mask-image: url("/images/eye.svg");
+
+    &:hover {
+      background-color: $accent;
+    }
+  }
+  &__toggle {
+    font-size: 12px;
+    align-self: flex-start;
+    margin: 0 0 10px 7%;
+    color: $text-secondary;
+    cursor: pointer;
   }
   &__button {
     margin: 10px 0 0 0;
